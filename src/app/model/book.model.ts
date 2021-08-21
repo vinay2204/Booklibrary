e125072b363c8287dd41779c5db9322be0243f02
Vinay kumar <vinay.k.rohilla@gmail.com>
2021-08-21 11:25:46 +0530

    book model update

---
+++ src/app/model/book.model.ts
@@ -0,0 +1,19 @@
+export class BookDetailsModel{
+    "bookName": string;
+    "authorName": string;
+    "language": string;
+    "published": number;
+    "bookcount":number;
+    "bookId": string;
+
+
+    constructor(){
+        this.bookName ='';
+        this.authorName = '';
+        this.language ='';
+        this.bookId ='';
+       
+
+        }
+
+}
